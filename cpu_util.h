ed897e53e6a8f371f407490a6a7dbbd85f326d8e
Alexander Zinovyev <zigmatulin@gmail.com>
2021-02-15 20:33:40 +1300

    RISC-V build support (cpu_relax in cpu_util.h)

--- cpu_util.h
+++ cpu_util.h
@@ -42,6 +42,8 @@ static inline void cpu_relax(void) {
 }
 #elif defined(__aarch64__)
 # define cpu_relax() asm volatile("yield" ::: "memory")
+#elif defined(__riscv)
+#define cpu_relax() asm volatile("nop")
 #else
 #warning "no cpu_relax for your cpu"
 #define cpu_relax() do {} while (0)
